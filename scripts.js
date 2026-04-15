fc241643ade3ab79df7b915cc24c12a3124c429e
yorch <jorgeluis.herp>
2026-04-14 21:51:13 -0400

    añadido boton de nueva cuadricula

--- scripts.js
+++ scripts.js
@@ -9,20 +9,23 @@
 // estado inicial: hover negro
 let randomMode = false;
 
-function createGrid(gridSize){
-//  - Definir tamaño fijo del contenedor (ej: 960px).
+function createGrid(gridSize) {
+    //  - Definir tamaño fijo del contenedor (ej: 960px).
     const container = document.querySelector("#container");
+    container.innerHTML = "";
     const cellSize = 960 / gridSize;
-//    - Usar bucles anidados:
+    //    - Usar bucles anidados:
     //filas
-    for (let fila = 0; fila < gridSize; fila++){
-    //columnas:
-    for(let col = 0; col < gridSize; col++) {
-        const cell = document.createElement("div");
+    for (let fila = 0; fila < gridSize; fila++) {
+        //columnas:
+        for (let col = 0; col < gridSize; col++) {
+            const cell = document.createElement("div");
             cell.classList.add("cell");
+            cell.style.width = cellSize + "px";
+            cell.style.height = cellSize + "px";
             //hover > depende del modo
             cell.addEventListener("mouseover", () => {
-                if(randomMode) {
+                if (randomMode) {
                     const r = Math.floor(Math.random() * 256);
                     const g = Math.floor(Math.random() * 256);
                     const b = Math.floor(Math.random() * 256);
@@ -35,15 +38,29 @@ function createGrid(gridSize){
         }
     }
 }
+//boton para alternar colores
 const toggleButton = document.querySelector("#toggleColors");
 toggleButton.addEventListener("click", () => {
     randomMode = !randomMode;
     toggleButton.textContent = randomMode
-    ? "Desactivar colores aleatorios"
-    : "Activar colores aleatorios";
+        ? "Desactivar colores aleatorios"
+        : "Activar colores aleatorios";
 });
+//boton para resetear cuadricula
+const newGridButton = document.querySelector("#newGrid");
+newGridButton.addEventListener("click", () => {
+    let gridSize = prompt("Ingresa el tamaño de la grilla(maximo 100)");
+    gridSize = parseInt(gridSize);
 
+    if (gridSize > 0 && gridSize <= 100) {
+        createGrid(gridSize);
+    } else {
+        alert("Numero invalido. debe ser entre 1 y 100");
+    }
+});
+//iniciar 
 createGrid(16)
+
 // 3. Hover effect
 //    - Detectar evento "mouseover" en cada celda.
 //    - Cambiar color de fondo de la celda al pasar el mouse.
